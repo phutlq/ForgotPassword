f6f65b25a2424f5e125133c38680e580a7041b2b
Tran Le Quang Phu <phutlq@greenglobal.vn>
2017-02-13 22:59:42 +0700

    Comit
    change

--- app/src/main/java/com/example/pc/forgotpassworddemo/Constant.java
+++ app/src/main/java/com/example/pc/forgotpassworddemo/Constant.java
@@ -9,5 +9,7 @@ public class Constant {
     public static final String MAIN_URL = "http://luxury-api.greenglobal.vn:9920";
     public static final String FORGOT_PASS_URL = "/password/forgot/request";
     public static final String FIELD = "email";
-
+    public static final String TITTLE_TEXT = "DEMO";
+    public static final String FORGOT_TEXT = "FORGOT YOUR PASSWORD";
+    public static final String BUTTON_FORGOT_TEXT = "Forgot";
 }
--- app/src/main/java/com/example/pc/forgotpassworddemo/MainActivity.java
+++ app/src/main/java/com/example/pc/forgotpassworddemo/MainActivity.java
@@ -23,11 +23,11 @@ public class MainActivity extends AppCompatActivity {
         mForgotPassword.setBackgroundColor(R.drawable.sharpe_gradient);
         mForgotPassword.setButtonBackIcon(R.drawable.ic_back);
         mForgotPassword.setVisibleButtonBack(View.VISIBLE);
-        mForgotPassword.setTittleText("DEMO");
-        mForgotPassword.setForgotText("FORGOT YOUR PASSWORD");
+        mForgotPassword.setTittleText(Constant.TITTLE_TEXT);
+        mForgotPassword.setForgotText(Constant.FORGOT_TEXT);
         mForgotPassword.setEdittextEmailBackground(R.drawable.edittext_border);
         mForgotPassword.setButtonForgotBackground(R.drawable.button_background);
-        mForgotPassword.setButtonForgotText("Forgot");
+        mForgotPassword.setButtonForgotText(Constant.BUTTON_FORGOT_TEXT);
         mForgotPassword.setButtonNextIcon(R.drawable.ic_next_main);
         mForgotPassword.sendRequestForgotPassword(MainActivity.this,Constant.MAIN_URL,Constant.MAIN_URL+Constant.FORGOT_PASS_URL);
 
